333f5e31aa84c6fec63642ebf5618d0090e3faf4
Ninqwelosse <gritskikh.aa@phystech.edu>
2022-03-22 16:21:29 +0300

    hw from master to main

---
+++ homework2/SMARTpointers/Stack.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+void printStack(stack<int>& a) {
+	stack <int> b = a;
+	stack <int> c;
+	for (int i = 0; i < a.size(); i++) {
+		c.push(b.top());
+		//cout << c.top() << "\n";
+		b.pop();
+	}
+	cout << c.size() << "---\n";
+	for (int i = 0; i < c.size();i++) {
+		cout << c.top() << "--" << i << "\n";
+		c.pop();
+	}
+}
+using namespace std;
+
+int main() {
+	stack <int> v;
+	for (int i = 0; i < 18;i++) {
+		v.push(i);
+	}
+	printStack(v);
+
+	return 0;
+
+}
