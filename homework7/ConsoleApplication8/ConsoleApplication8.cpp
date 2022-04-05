c4f8d6bdc6ea86506b83cd21ae6659cf6d737b7d
Ninqwelosse <gritskikh.aa@phystech.edu>
2022-04-05 19:11:18 +0300

    json

---
+++ homework7/ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,33 @@
+﻿// ConsoleApplication8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <vector>
+#include<string>
+#include <ostream>
+#include "json.hpp"
+
+using nlohmann::json;
+
+int main(){
+	system("chcp 65001");
+	std::filesystem::path path_input = "person.json";
+	std::fstream fin(path_input.string(), std::ios::out);
+
+	
+	json map;
+	std::string a, b, c;
+	std::cin >> a >> b >> c;
+	map["Name"] = a;
+	map["Surname"] = b;
+	map["Age"] = c;
+    
+	fin << map;
+
+	fin.close();
+
+}
+
+ 
