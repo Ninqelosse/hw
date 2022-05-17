a8ed6a328d708763d3cb7fa182521f3c29027bfc
Aleksandra <74494905+Ninqelosse@users.noreply.github.com>
2022-05-17 18:00:12 +0300

    Add files via upload

---
+++ homework7/last one.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+
+using namespace std;
+using namespace std::filesystem;
+using namespace std::chrono_literals;
+
+
+
+	
+void view_directory(const path& path){
+	if (exists(path) && is_directory(path)){
+		for (const auto& entry : directory_iterator(path)){
+			auto file_name = entry.path().filename().string();
+
+			cout << file_name << endl;
+			
+			auto ftime = last_write_time(file_name);
+			
+			const auto systemTime = std::chrono::clock_cast<std::chrono::system_clock>(ftime);
+			const auto time = std::chrono::system_clock::to_time_t(systemTime);
+			cout << time << endl;
+		}
+	}
+}
+int main(){
+	string A;
+	cin >> A;
+	path path = A;
+	view_directory(path);
+}
